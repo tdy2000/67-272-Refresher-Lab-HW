6709c9e3e5119d026ff22b17571484569ec2af17
Thomas Yager <tdy@andrew.cmu.edu>
2021-02-06 12:04:21 -0500

    All tests passing for Child model ... except for missing Chore model

--- app/models/child.rb
+++ app/models/child.rb
@@ -4,5 +4,12 @@ class Child < ApplicationRecord
     has_many :tasks, through: :chores
 
     validates_presence_of :first_name, :last_name
-    
+
+    scope :alphabetical, -> { order('last_name, first_name') }
+    scope :active, -> { where(active: true) }
+
+    def name
+        first_name + " " + last_name
+    end
+
 end
