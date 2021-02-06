7e69a61814fae88956867ca0b6d0fb9a7a672462
Thomas Yager <tdy@andrew.cmu.edu>
2021-02-06 13:03:06 -0500

    All Chore Model tests pass

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -3,4 +3,17 @@ class Chore < ApplicationRecord
   belongs_to :child
   belongs_to :task
   
+  validates_date :due_on
+
+  scope :by_task, -> { joins(:task).order('name') }
+  scope :chronological, -> { order('due_on, completed') }
+  scope :pending, -> { where(completed: false) }
+  scope :done, -> { where(completed: true) }
+  scope :upcoming, -> { where('due_on >= ?', Date.today) }
+  scope :past, -> { where('due_on < ?', Date.today) }
+
+  def status
+    completed ? "Completed" : "Pending"
+  end
+
 end
