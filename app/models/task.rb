88c9e69dee92d9f9ae8ec224d13afa5329cbe3d6
Thomas Yager <tdy@andrew.cmu.edu>
2021-02-06 12:29:25 -0500

    Got Task model working, all tests pass, added belongs code to the Chores model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,13 @@
 class Task < ApplicationRecord
+
+    has_many :chores
+    has_many :children, through: :chores
+
+    validates_presence_of :name
+    # points need to be a positive integer, unclear whether a chore can be worth zero
+    # points, so I have decided to allow it
+    validates_numericality_of :points, only_integer: true, greater_than: -1
+
+    scope :alphabetical, -> { order('name') }
+    scope :active, -> { where(active: true) }
 end
